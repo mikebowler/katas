10457e20bffb161aee56e911dd51650f7b8fe71d
Mike Bowler <mbowler@gargoylesoftware.com>
2021-07-18 09:39:39 -0700

    changed initial tester

--- fizzbuzz/java-junit/FizzBuzzTest.java
+++ fizzbuzz/java-junit/FizzBuzzTest.java
@@ -1,6 +1,13 @@
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class FizzBuzz {
-    public String convert(int number) {
-        return "Not implemented";
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class FizzBuzzTest {
+
+    @Test
+    void printAllResults() {
+        for( int i=1; i<=100; i++ ) {
+            System.out.println("convert(" + i + ")=" + FizzBuzz.convert(i));
+        }
     }
 }
