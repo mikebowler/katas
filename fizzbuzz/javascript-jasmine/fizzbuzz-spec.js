4940da923ce5c4244987e758db5ec913c9642b30
Mike Bowler <mbowler@gargoylesoftware.com>
2021-07-24 10:23:53 -0700

    Improved naming

--- fizzbuzz/javascript-jasmine/fizzbuzz-spec.js
+++ fizzbuzz/javascript-jasmine/fizzbuzz-spec.js
@@ -2,8 +2,8 @@
 
 const fizzbuzz = require('./fizzbuzz.js');
 
-describe('answer', () => {
-  it('to life the universe and everything', () => {
+describe('fizzbuzz', () => {
+  it('print all', () => {
     for(var i=1;i<=100;i++) {
       console.log("fizzbuzz("+i+")="+fizzbuzz.fizzbuzz(i));
     }
